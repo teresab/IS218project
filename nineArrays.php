5e6850a7bf7ca34456fdb7b948bcba3c129a2156
Teresa Bembury <tb94@njit.edu>
2016-10-29 13:19:45 -0400

    data usage

--- nineArrays.php
+++ nineArrays.php
@@ -89,6 +89,88 @@ Array
     [5] => test.txt
 )
 */
+$files = glob('../images/a*.jpg');
+ 
+print_r($files);
+/* output looks like:
+Array
+(
+    [0] => ../images/apple.jpg
+    [1] => ../images/art.jpg
+)
+*/
 
+$files = glob('../images/a*.jpg');
+ 
+// applies the function to each array element
+$files = array_map('realpath',$files);
+ 
+print_r($files);
+/* output looks like:
+Array
+(
+    [0] => C:\wamp\www\images\apple.jpg
+    [1] => C:\wamp\www\images\art.jpg
+)
+*/
+
+echo "Initial: ".memory_get_usage()." bytes \n";
+/* prints
+Initial: 361400 bytes
+*/
+ 
+// let's use up some memory
+for ($i = 0; $i < 100000; $i++) {
+    $array []= md5($i);
+}
+ 
+// let's remove half of the array
+for ($i = 0; $i < 100000; $i++) {
+    unset($array[$i]);
+}
+ 
+echo "Final: ".memory_get_usage()." bytes \n";
+/* prints
+Final: 885912 bytes
+*/
+ 
+echo "Peak: ".memory_get_peak_usage()." bytes \n";
+/* prints
+Peak: 13687072 bytes
+*/
+
+// sleep for 3 seconds (non-busy)
+sleep(3);
+ 
+$data = getrusage();
+echo "User time: ".
+    ($data['ru_utime.tv_sec'] +
+    $data['ru_utime.tv_usec'] / 1000000);
+echo "System time: ".
+    ($data['ru_stime.tv_sec'] +
+    $data['ru_stime.tv_usec'] / 1000000);
+ 
+/* prints
+User time: 0.011552
+System time: 0
+*/
+
+// loop 10 million times (busy)
+for($i=0;$i<10000000;$i++) {
+ 
+}
+ 
+$data = getrusage();
+echo "User time: ".
+    ($data['ru_utime.tv_sec'] +
+    $data['ru_utime.tv_usec'] / 1000000);
+echo "System time: ".
+    ($data['ru_stime.tv_sec'] +
+    $data['ru_stime.tv_usec'] / 1000000);
+ 
+/* prints
+User time: 1.424592
+System time: 0.004204
+*/
 </body>
 </html>
