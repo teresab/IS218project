4cce219bedd10632072bab4b29c2e20b6078e814
Teresa Bembury <tb94@njit.edu>
2016-10-29 14:07:58 -0400

    shutdown function

--- nineArrays.php
+++ nineArrays.php
@@ -284,5 +284,22 @@ $start_time = microtime(true);
 echo "execution took: ".
         (microtime(true) - $start_time).
         " seconds.";
+        
+        
+        $start_time = microtime(true);
+ 
+register_shutdown_function('my_shutdown');
+ 
+// do some stuff
+// ...
+ 
+ 
+function my_shutdown() {
+    global $start_time;
+ 
+    echo "execution took: ".
+            (microtime(true) - $start_time).
+            " seconds.";
+}
 </body>
 </html>
